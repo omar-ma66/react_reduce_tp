308f3a6d39873faa6448cda92225719478b8bd70
omar-ma66 <devcodefree66@gmail.com>
2026-09-07 16:34:27 +0200

    voila certaine modiff fonctionnnes

--- src/components/PlayerList.jsx
+++ src/components/PlayerList.jsx
@@ -1,17 +1,27 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import PlayerCard from "./PlayerCard.jsx";
 
 function PlayerList() {
-  const [players] = useState({
-    1: { name: "John", pv: 100, pvMax: 100, mana: 30, manaMax: 30, id: 1 },
-    2: { name: "Jack", pv: 100, pvMax: 100, mana: 30, manaMax: 30, id: 2 },
-    3: { name: "Jessy", pv: 100, pvMax: 100, mana: 30, manaMax: 30, id: 3 },
-    4: { name: "Jenny", pv: 100, pvMax: 100, mana: 30, manaMax: 30, id: 4 },
-  });
+  // const [players] = useState({
+  //   1: { name: "John", pv: 100, pvMax: 100, mana: 30, manaMax: 30, id: 1 },
+  //   2: { name: "Jack", pv: 100, pvMax: 100, mana: 30, manaMax: 30, id: 2 },
+  //   3: { name: "Jessy", pv: 100, pvMax: 100, mana: 30, manaMax: 30, id: 3 },
+  //   4: { name: "Jenny", pv: 100, pvMax: 100, mana: 30, manaMax: 30, id: 4 },
+  // }); 
 
+const players = useSelector((state)=>state.fight.players) ;
+
+if(!players )
+{
+  return ( <div>Chargement des joueurs ...</div>)
+}
+
+const playerList = Array.isArray(players) ? players : Object.values(players) ;
   return (
     <div className="row">
-      {Object.values(players).map((player) => (
+      {playerList.map((player) => (
         <PlayerCard key={player.id} player={player} />
       ))}
     </div>
--- src/features/fight/fightSlice.js
+++ src/features/fight/fightSlice.js
@@ -1,13 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  players: [
+  players: 
     // Nous stockerons nos combattants ici sous forme de tableau
     // Exemple: 1: { name: "John", pv: 100, pvMax: 100, mana: 30, manaMax: 30, id: 1 },
-  ],
+    {
+     1:{ name: "John", pv: 100, pvMax: 100, mana: 30, manaMax: 30, id: 1 },
+     2:{ name: "Jack", pv: 100, pvMax: 100, mana: 30, manaMax: 30, id: 2 },
+     3:{ name: "Jessy", pv: 100, pvMax: 100, mana: 30, manaMax: 30, id: 3 },
+     4:{ name: "Jenny", pv: 100, pvMax: 100, mana: 30, manaMax: 30, id: 4 },
+  }
+  ,
   monster: {
     // Notre boss à combattre
     // Exemple: { name: "Dragon", pv: 200, pvMax: 200, strength: 15 }
+    nom: "Crypto",
+    pv:800,
+    pvMax:800
   },
 };
 
